919349f46127dbdf3ff499e59e66cdd56f800828
sudhanshu uniyal <uniyal1001@gmail.com>
2019-12-10 00:23:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}  from 'react';
+import { render } from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      listOfTodo: []
+
+    };
+  }
+  handleChangeOfName = event => {
+    console.log(this.state.name);
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  addToDo = event => {
+    this.setState({
+      listOfTodo: [...this.state.listOfTodo, this.state.name],
+      name: ""
+    })
+  }
+  delete = (value) =>{
+    alert(value);
+    let filtered = this.state.listOfTodo.filter((eachItem) => {
+      return eachItem != value
+    }
+    )
+    this.setState({
+      listOfTodo: filtered
+    })
+  }
+  render() {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start editing to see some magic happen :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input
+          type="text"
+          onChange={event => this.handleChangeOfName(event)}
+          value={this.state.name}
+          placeholder="Enter name..."
+        />
+        <button onClick={this.addToDo}> Add </button>
+                <h1>Value  {this.state.name}</h1>
+{
+  this.state.listOfTodo.map(eachElm => (
+    <li>{eachElm} <button onClick={() => this.delete(eachElm)}>Delete</button></li>
+  ))
+}
+      </div>
+    );
+  }
 }
 
 export default App;
